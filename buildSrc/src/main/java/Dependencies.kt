a4e0b010ef38d716dd3cf9a3d801994bf05c9d96
Arthur Saad <arthursaadb@gmail.com>
2019-11-13 22:56:37 -0300

    First commit

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,54 @@
+object Dependencies {
+    const val kotlin_stdlib_jdk7 = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+
+    const val core = "androidx.core:core-ktx:${Versions.core}"
+
+    const val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    const val constraint_layout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    const val card_view = "androidx.cardview:cardview:${Versions.cardview}"
+    const val recyclerview = "androidx.recyclerview:recyclerview:${Versions.recyclerview}"
+    const val material = "com.google.android.material:material:${Versions.material}"
+    const val annotation = "androidx.annotation:annotation:${Versions.annotation}"
+
+    const val navigation_ui = "androidx.navigation:navigation-ui-ktx:${Versions.navigation_ui_ktx}"
+    const val navigation_fragment = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation_fragment_ktx}"
+
+    const val lifecycle_extensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle_extensions}"
+    const val lifecycle_compiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle_compiler}"
+    const val lifecycle_viewmodel_ktx = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle_viewmodel_ktx}"
+    const val lifecycle_livedata_ktx = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle_livedata_ktx}"
+    const val lifecycle_runtime = "androidx.lifecycle:lifecycle-runtime:${Versions.lifecycle_runtime}"
+
+    const val kotlin_coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.kotlin_coroutines}"
+
+    const val timber = "com.jakewharton.timber:timber:${Versions.timber}"
+
+    const val rxjava = "io.reactivex.rxjava3:rxjava:${Versions.rxjava}"
+    const val rxandroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxandroid}"
+
+    const val room_ktx = "androidx.room:room-ktx:${Versions.room_ktx}"
+    const val room_runtime = "androidx.room:room-runtime:${Versions.room_runtime}"
+    const val room_compiler = "androidx.room:room-compiler:${Versions.room_runtime}"
+
+    const val koin_android = "org.koin:koin-android:${Versions.koin_android}"
+    const val koin_viewmodel = "org.koin:koin-androidx-viewmodel:${Versions.koin_viewmodel}"
+    const val koin_scope = "org.koin:koin-androidx-scope:${Versions.koin_scope}"
+
+    const val shimmer =  "com.facebook.shimmer:shimmer:${Versions.shimmer}"
+
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val retrofit_rx_adapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit_rx_adapter}"
+    const val retrofit_converter_gson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit_converter_gson}"
+    const val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
+    const val okhttp_urlconnection = "com.squareup.okhttp3:okhttp-urlconnection:${Versions.okhttp_urlconnection}"
+    const val okhttp_logging_interceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp_loggin_interceptor}"
+
+    const val testlib_junit = "junit:junit:${Versions.junit}"
+
+    const val testandroidx_rules = "androidx.test:rules:${Versions.androidx_testing}"
+    const val testandroidx_runner = "androidx.test:runner:${Versions.androidx_testing}"
+    const val testandroidx_espressocore = "androidx.test.espresso:espresso-core:${Versions.androidx_espresso}"
+
+    const val tools_gradle_android = "com.android.tools.build:gradle:${Versions.gradleandroid}"
+    const val tools_kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,64 @@
+object Versions {
+    //TOOLS
+    const val gradleandroid = "3.5.0"
+    const val kotlin = "1.3.50"
+
+    //ANDROIDX
+    const val core = "1.0.2"
+    const val appcompat = "1.0.2"
+    const val recyclerview = "1.0.0"
+    const val constraintLayout = "1.1.3"
+    const val material = "1.1.0-alpha04"
+    const val cardview = "1.0.0"
+    const val annotation = "1.1.0"
+
+    const val navigation_ui_ktx = "2.0.0"
+    const val navigation_fragment_ktx = "2.0.0"
+
+    // COROUTINES
+    const val kotlin_coroutines = "1.3.1"
+
+    // LIFECYCLES
+    const val lifecycle_extensions = "2.2.0-alpha04"
+    const val lifecycle_compiler = "2.2.0-alpha04"
+    const val lifecycle_viewmodel_ktx = "2.2.0-alpha03"
+    const val lifecycle_livedata_ktx = "2.2.0-alpha03"
+    const val lifecycle_runtime = "2.2.0-alpha04"
+
+    //PAGING
+    const val paging_runtime = "2.1.0"
+
+    //TIMBER
+    const val timber = "4.7.1"
+
+    //RETROFIT
+    const val retrofit = "2.6.1"
+    const val retrofit_rx_adapter = "2.4.0"
+    const val retrofit_converter_gson = "2.6.1"
+
+    //OKHTTP
+    const val okhttp = "4.2.0"
+    const val okhttp_urlconnection = "4.2.0"
+    const val okhttp_loggin_interceptor = "4.2.0"
+
+    //SHIMMER
+    const val shimmer = "0.4.0"
+
+    //ROOM
+    const val room_ktx = "2.2.0-rc01"
+    const val room_runtime = "2.2.0-rc01"
+
+    //RXJava & RXAndroid
+    const val rxjava = "3.0.0-RC2"
+    const val rxandroid = "2.1.1"
+
+    //KOIN
+    const val koin_android = "2.0.1"
+    const val koin_viewmodel = "2.0.1"
+    const val koin_scope = "2.0.1"
+
+    //TEST
+    const val junit = "4.12"
+    const val androidx_espresso = "3.1.1"
+    const val androidx_testing = "1.1.1"
+}
